c9cbf44696f80afbecd377d1e89fb0b1b3547fee
DeLaut <54510364+DeLaut@users.noreply.github.com>
2019-12-08 22:24:10 +0300

    fixed: horizontal scroll in mobile and tablet version; changed way of show/hide of sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,31 +1,11 @@
-function changeBurgerState() {
-    if (burger.className == "burger burger_active") {
-        burger.className = "burger";
-    }
-        else {
-            burger.className = "burger burger_active";
-        }
+
+var aside = document.querySelector('.aside');
+var burger = document.querySelector('.burger');
+var cleaner = document.querySelector('.aside__cleaner');
+
+function changeState() {
+    aside.classList.toggle("aside_hidden");
 }
 
-function changeAsideState() {
-    if (aside.className == "aside aside_active") {
-        aside.className = "aside";
-    }
-        else {
-            aside.className = "aside aside_active";
-        }
-}
-
-// function onClickClose(event) {
-//     if (!aside.contains(event.target) && aside.className == "aside_active") {
-//         aside.className = "aside";
-//         burger.className = "burger";
-//     }
-// }
-
-let burger = document.querySelector(".burger");
-let aside = document.querySelector(".aside");
-
-burger.addEventListener("click", changeBurgerState);
-burger.addEventListener("click", changeAsideState);
-// document.addEventListener("click", onClickClose);
+burger.addEventListener("click", changeState);
+cleaner.addEventListener("click", changeState);
